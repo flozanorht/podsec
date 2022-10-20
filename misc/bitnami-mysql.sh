296e5dc696d303f6eb40846adf19fddf42363918
Fernando Lozano <flozano@redhat.com>
2022-10-20 15:38:17 -0300

    working example using a bitnami chart

--- misc/bitnami-mysql.sh
+++ misc/bitnami-mysql.sh
@@ -1,11 +1,20 @@
 # installs mysql from a community chart
 
-# cannot start a pod, having pod security issues
-# it looks like that chart (or its image) doesn't like OpenShift SCC or recent pod security. :-(
+# without many values, it generates a pretty insecure pod security context and collides with both OpenShif SCC and Kubernetes pod security :-(
 
 # it does NOT delete its PVC on uninstall
 
 # helm repo add bitnami https://charts.bitnami.com/bitnami
 
-helm install mysql --set auth.database=db --set auth.username=user --set auth.password=password  bitnami/mysql
+helm install mysql --version=9.3.5 \
+  --set auth.database=db --set auth.username=user --set auth.password=password \
+  --set primary.podSecurityContext.enabled=false --set primary.containerSecurityContext.enabled=false \
+  --set serviceAccount.create=false \
+  --set primary.persistence.size=100Mi \
+  bitnami/mysql
+
+# add  primary.resources.requests
+
+# helm upgrade mysql bitnami/mysql --version=9.4.1
+
 
