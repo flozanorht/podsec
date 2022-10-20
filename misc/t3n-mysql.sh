1fdacaa930527bdb58897d912ab3f59fd8579f19
Fernando Lozano <flozano@redhat.com>
2022-10-20 13:20:38 -0300

    first attempt at helm upgrade. it failed

--- misc/t3n-mysql.sh
+++ misc/t3n-mysql.sh
@@ -5,14 +5,21 @@
 # also looks like the user it creates does NOT own the db
 # at least it does create a PVC and removes it on unistall -- which may NOT be good for production.
 
+# explictly not using the latest version of the chart so I can test updates
+# LoL my upgrade attempt got a CrashLoopBackOff from "InnoDB: Assertion failure" errors
+# it looks like the chart was not designed to handle upgrades. :-(
+# interesting that the latest release worked fine on first install
+
 # helm repo add t3n https://storage.googleapis.com/t3n-helm-charts
 
-helm install mysql t3n/mysql --version 1.0.0 \
+helm install mysql t3n/mysql --version 0.1.0 \
   --set mysqlUser=user \
   --set mysqlPassword=password \
   --set mysqlDatabase=db \
   --set mysqlRootPassword=redhat \
   --set persistence.size=100Mi
+
+# helm upgrade mysql t3n/mysql --version 1.0.0
 
 # when the db is running:
 # oc exec -it $(oc get pod -o name -l app.kubernetes.io/name=mysql) -- mysql -uroot -p
