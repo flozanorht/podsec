b0eee2cd06907f263014599f893bd12db540dd65
Fernando Lozano <flozano@redhat.com>
2022-10-20 12:51:17 -0300

    initial attempts at using community helm charts

---
+++ misc/bitnami-mysql.sh
@@ -0,0 +1,9 @@
+# installs mysql from a community chart
+
+# cannot start a pod, having pod security issues
+# it looks like that chart (or its image) doesn't like OpenShift SCC or recent pod security. :-(
+
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+
+helm install mysql --set auth.database=db --set auth.username=user --set auth.password=password  bitnami/mysql
+
---
+++ misc/t3n-mysql.sh
@@ -0,0 +1,19 @@
+# I think this chart repo is deprecated and not maintained anymore, but the bitnami chart is way too complex :-(
+
+# could specify resource limits and PVC size
+# looks like the DB starts with no grants for remote access :-(
+# also looks like the user it creates does NOT own the db
+
+# helm repo add t3n https://storage.googleapis.com/t3n-helm-charts
+
+helm install mysql t3n/mysql --version 1.0.0 \
+  --set mysqlUser=user \
+  --set mysqlPassword=password \
+  --set mysqlDatabase=db \
+  --set mysqlRootPassword=redhat \
+  --set persistence.size=100Mi
+
+# when the db is running:
+# oc exec -it $(oc get pod -o name -l app.kubernetes.io/name=mysql) -- mysql -uroot -p
+# grant all on db.* to user@'%';
+
